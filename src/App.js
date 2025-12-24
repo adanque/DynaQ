8c9fe3dc8e3e9d5b42453454b617a48214b7193d
Alan Danque <adanque@gmail.com>
2025-12-24 10:29:48 -0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 function App() {
   const [messages, setMessages] = useState([]);
   // const [message, setMessage] = useState('');
-  const [reply, setReply] = useState('');
+  // const [reply, setReply] = useState('');
   const [input, setInput] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -21,7 +21,7 @@ function App() {
     try {
 
       const response = await axios.post('https://dynaq.azurewebsites.net/api/dynaq_chat', { messages });
-      setReply(response.data.reply);      
+      // setReply(response.data.reply);      
 
 
       // const response = await fetch('https://dynaq.azurewebsites.net/api/dynaq?code=QZA3J8KqSM86PwoXJJEBU5NcMB_QeOKT-NK3cy5ptCi0AzFu7SSnLQ%3D%3D&name=alan', {
@@ -36,7 +36,7 @@ function App() {
       const botMessage = { text: data.response || 'No response from bot', sender: 'bot' };
       setMessages((prev) => [...prev, botMessage]);
     } catch (error) {
-      setReply('Error: ' + error.message);
+      // setReply('Error: ' + error.message);
       const errorMessage = { text: 'Error: ' + error.message, sender: 'bot' };
       setMessages((prev) => [...prev, errorMessage]);
     } finally {
