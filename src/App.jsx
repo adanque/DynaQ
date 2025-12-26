cfa6dfba79881300452adff5b69f25b0f7c04dc5
Alan Danque <adanque@gmail.com>
2025-12-26 16:06:03 -0600

    updates for corrections

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,6 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-// const apikeyv="jHyIcXcJmfAmF14uhBRevFHepTYr_fn4J6aLIP2edmD5AzFubs4mUA%3D%3D"
-// console.log(import.meta.env.VITE_AZURE_FUNCTION_KEY);
 
 // Register Chart.js components
 ChartJS.register(
@@ -49,19 +47,6 @@ function App() {
   };
 
 
-  // State for chat bot example
-  // const [chatMessages, setChatMessages] = useState([]);
-  // const [input, setInput] = useState('');
-
-
-  // const handleSend = () => {
-  //   if (input.trim()) {
-  //     setChatMessages([...chatMessages, { text: input, sender: 'user' }]);
-  //     // Simulate bot response (replace with real API call)
-  //     setChatMessages((prev) => [...prev, { text: 'Bot reply: ' + input, sender: 'bot' }]);
-  //     setInput('');
-  //   }
-  // };
   const handleSend = async (e) => {
     e.preventDefault(); // Prevent form submission reload
     if (!input.trim()) return;
@@ -175,39 +160,3 @@ function App() {
 
 export default App;
 
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
